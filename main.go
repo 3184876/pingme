642d9c7d6afbcca82b4746777d03ab4d2c7b5359
noobly314 <noobly314@gmail.com>
2019-12-13 00:28:23 +0800

    use logrus

--- main.go
+++ main.go
@@ -7,22 +7,10 @@ import (
 	"regexp"
 	"strings"
 	"time"
-
-	"github.com/fatih/color"
 )
 
-// Global Variables
-var fmtOK *color.Color
-var fmtInfo *color.Color
-var fmtWarn *color.Color
-var fmtError *color.Color
-
 func init() {
-	// Set colorful fmt
-	fmtOK = color.New(color.FgGreen).Add(color.Bold)
-	fmtInfo = color.New(color.FgBlue).Add(color.Bold)
-	fmtWarn = color.New(color.FgYellow).Add(color.Bold)
-	fmtError = color.New(color.FgRed).Add(color.Bold)
+	init_log()
 }
 
 func main() {
@@ -31,25 +19,25 @@ func main() {
 
 	switch len(flag.Args()) {
 	case 0:
-		//fmtWarn.Println("Please specify target.")
+		Log.Warn("Please specify target.")
 		helper()
 	case 1:
 		addr := flag.Args()[0]
 		ip := lookupIP(addr)
-		tcpPing(ip, "22")
-		tcpPing(ip, "80")
-		tcpPing(ip, "443")
+		tcping(ip, "22")
+		tcping(ip, "80")
+		tcping(ip, "443")
 	case 2:
 		addr := flag.Args()[0]
 		port := flag.Args()[1]
 		ip := lookupIP(addr)
-		tcpPing(ip, port)
+		tcping(ip, port)
 	default:
-		fmtWarn.Println("Too many arguments.")
+		Log.Warn("Too many arguments.")
 	}
 }
 
-func catDialErr(err error) string {
+func getDialError(err error) string {
 	match, _ := regexp.MatchString("timeout", err.Error())
 	if match {
 		return "timeout"
@@ -69,42 +57,19 @@ func lookupIP(addr string) string {
 	return ips[0].String()
 }
 
-func tcpPing(ip string, port string) {
-	tcpAddr := strings.Join([]string{ip, ":", port}, "")
+func tcping(ip string, port string) {
+	address := strings.Join([]string{ip, ":", port}, "")
 	d := net.Dialer{Timeout: 3 * time.Second}
-	_, err := d.Dial("tcp", tcpAddr)
+	_, err := d.Dial("tcp", address)
 	if err != nil {
-		errType := catDialErr(err)
+		errType := getDialError(err)
 		if errType == "refused" {
-			fmtInfo.Printf(tcpAddr)
-			fmtWarn.Printf(" is reachable, but the port is closed.\n")
+			Log.Warn("CLOSED    ", address)
 		}
 		if errType == "timeout" {
-			fmtInfo.Printf(tcpAddr)
-			fmtError.Printf(" is unreachable.\n")
+			Log.Warn("ERROR     ", address)
 		}
 	} else {
-		fmtInfo.Printf(tcpAddr)
-		fmtOK.Printf(" is reachable.\n")
+		Log.Info("OPEN      ", address)
 	}
-}
-
-func helper() {
-	fmtInfo.Println("PingMe helper:")
-	fmt.Println()
-	fmtInfo.Println("1. Basic usage: pingme <ip> <port>")
-	fmt.Println("e.g. pingme google.com 443")
-	fmt.Println()
-	fmtInfo.Println("2. Input ip only: pingme <ip>")
-	fmt.Println("e.g. pingme facebook.com")
-	fmt.Println("PingMe will scan 3 common ports: 22, 80, 443")
-	fmt.Println()
-	fmtInfo.Println("3. Input URL: pingme <url>")
-	fmt.Println("e.g. pingme https://twitter.com")
-	fmt.Println("PingMe will scan 80 for http / 443 for https")
-	fmt.Println()
-	fmtInfo.Println("4. Add info flag: pingme -i <ip>")
-	fmt.Println("e.g. pingme -i https://wikipedia.org")
-	fmt.Println("PingMe will query detailed information about ip from https://pingme.cc")
-	fmt.Println()
 }
