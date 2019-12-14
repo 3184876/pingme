4944730e7da4ddd58331353909456ef342e62462
noobly314 <noobly314@gmail.com>
2019-12-14 20:34:28 +0800

    add parseInput & isValidIP

--- tcping/tcping.go
+++ tcping/tcping.go
@@ -10,13 +10,14 @@ func New(address string) int {
 	d := net.Dialer{Timeout: 3 * time.Second}
 	_, err := d.Dial("tcp", address)
 	if err != nil {
-		errType := getDialError(err)
-		if errType == "refused" {
+		match, _ := regexp.MatchString("refused", err.Error())
+		if match {
 			// Closed
 			return 1
 		}
-		if errType == "timeout" {
-			// Error
+		match, _ = regexp.MatchString("timeout", err.Error())
+		if match {
+			// Timeout
 			return 2
 		}
 	} else {
@@ -25,16 +26,4 @@ func New(address string) int {
 	}
 	// Default
 	return 2
-}
-
-func getDialError(err error) string {
-	match, _ := regexp.MatchString("timeout", err.Error())
-	if match {
-		return "timeout"
-	}
-	match, _ = regexp.MatchString("refused", err.Error())
-	if match {
-		return "refused"
-	}
-	return ""
 }
--- util.go
+++ util.go
@@ -1,14 +1,37 @@
 package main
 
 import (
-	"fmt"
 	"net"
+	"regexp"
 )
 
-func lookupIP(addr string) string {
-	ips, err := net.LookupIP(addr)
+func lookupIP(s string) string {
+	// is ip
+	if isValidIP(s) {
+		return s
+	}
+	// is hostname
+	hostname := parseInput(s)
+	ips, err := net.LookupIP(hostname)
 	if err != nil {
-		fmt.Println(err)
+		Log.Fatal("IP address not found.")
 	}
 	return ips[0].String()
+}
+
+func parseInput(s string) string {
+	var hostname string
+
+	re := regexp.MustCompile(`^https?\:\/\/([^\/:?#]+)(?:[\/:?#]|$)`)
+	results := re.FindStringSubmatch(s)
+	if len(results) == 2 {
+		hostname = results[1]
+	}
+
+	return hostname
+}
+
+func isValidIP(s string) bool {
+	re := regexp.MustCompile(`((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))`)
+	return re.Match([]byte(s))
 }
